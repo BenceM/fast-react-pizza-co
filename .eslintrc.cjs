eabca7be4d236b1e25105a10e622f24615dc8b09
Bence <m.bence211@gmail.com>
2023-11-19 19:59:25 +0100

    starter files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,7 @@
 module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
-	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:react/jsx-runtime",
-		"plugin:react-hooks/recommended",
-		"react-app",
-	],
+	extends: ["react-app"],
 	ignorePatterns: ["dist", ".eslintrc.cjs"],
 	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
 	settings: { react: { version: "18.2" } },
@@ -19,3 +13,7 @@ module.exports = {
 		],
 	},
 };
+// //"eslint:recommended",
+// "plugin:react/recommended",
+// "plugin:react/jsx-runtime",
+// "plugin:react-hooks/recommended",
