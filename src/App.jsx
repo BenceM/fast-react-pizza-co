9811ab20201916b613c4fc64a83b621505d0e9a9
Bence <m.bence211@gmail.com>
2023-11-22 18:32:21 +0100

    search order by id

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
@@ -23,7 +23,12 @@ const router = createBrowserRouter(
 				errorElement={<Error />}
 			/>
 			<Route path="/cart" element={<Cart />} />
-			<Route path="/order/:id" element={<Order />} />
+			<Route
+				path="/order/:id"
+				element={<Order />}
+				loader={orderLoader}
+				errorElement={<Error />}
+			/>
 			<Route path="/order/new" element={<CreateOrder />} />
 		</Route>,
 	),
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
+	const navigate = useNavigate();
 	const [query, setQuery] = useState("");
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		if (!query) return;
+		navigate(`/order/${query}`);
 	};
 	return (
 		<form onSubmit={handleSubmit}>
