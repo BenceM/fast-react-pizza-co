1aafd500e0c723e163a45befc5bd9922ee183428
Bence <m.bence211@gmail.com>
2023-11-25 20:34:01 +0100

    layout styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
 function AppLayout() {
-	const navigation = useNavigation();
-	console.log(navigation);
-	return (
-		<div className="layout">
-			{navigation.state === "loading" && <Loader />}
-			<Header />
-			<main>
-				{" "}
-				<Outlet />
-			</main>
-		</div>
-	);
+  const navigation = useNavigation();
+  console.log(navigation);
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {navigation.state === "loading" && <Loader />}
+      <Header />
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl ">
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
