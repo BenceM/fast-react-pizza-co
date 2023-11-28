3397ac418b8b15473f05a9ee662e4b5c6bfefdb9
Bence <m.bence211@gmail.com>
2023-11-28 19:56:46 +0100

    loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,6 +9,7 @@ function AppLayout() {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {navigation.state === "loading" && <Loader />}
+
       <Header />
       <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl ">
